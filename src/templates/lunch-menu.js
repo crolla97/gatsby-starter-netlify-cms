7f31e9c533da5c946645c4cbe6088d11c664711f
crolla97 <cristianocrolla@gmail.com>
2020-06-18 20:21:35 +0100

    Render all lunch menu item

--- src/templates/lunch-menu.js
+++ src/templates/lunch-menu.js
@@ -1,15 +1,62 @@
 import React from "react"
+import PropTypes from 'prop-types'
 import { graphql } from "gatsby"
+import Layout from '../components/Layout'
 
-const LunchMenu = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
+
+const LunchMenu = ({ data }) => {
+  
+  return (
+    <Layout>
+      <ul>
+        {data.markdownRemark.frontmatter.menu.stuzzichini.map((item) => (
+          <li>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <p>{item.price}</p>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {data.markdownRemark.frontmatter.menu.antipasti.map((item) => (
+          <li>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <p>{item.price}</p>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {data.markdownRemark.frontmatter.menu.primi.map((item) => (
+          <li>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <p>{item.price}</p>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  )
+}
 
 export const lunchQuery = graphql`
   {
     markdownRemark(frontmatter: {templateKey: {eq: "lunch-menu"}}) {
       id
       frontmatter {
+        templateKey
         menu {
           stuzzichini {
+            title
+            description
+            price
+          }
+          antipasti {
+            title
+            description
+            price
+          }
+          primi {
             title
             description
             price
